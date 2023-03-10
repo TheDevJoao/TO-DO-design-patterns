c11bd8e4ef71db8a4420ee7f9ca65ef054c09529
thedevjoao <thejoao94@gmail.com>
2023-03-10 13:01:54 -0300

    refactor: tasks, user controllers and serializers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,58 +4,22 @@ class TasksController < ApplicationController
   def index
     tasks = user.tasks
 
-    serialized_tasks = tasks.map do |task|
-      {
-        'id' => task.id,
-        'userId' => user.id,
-        'status' => task.status,
-        'name' => task.name,
-        'createdAt' => task.created_at,
-        'completedAt' => task.completed_at
-      }
-    end
-
-    render(json: serialized_tasks)
+    render(json: tasks.map { |task| TaskSerializer.new(task, user).as_json })
   end
 
   def show
     task = user.tasks.find(params[:id])
 
-    serialized_task = {
-      'id' => task.id,
-      'userId' => user.id,
-      'status' => task.status,
-      'name' => task.name,
-      'createdAt' => task.created_at,
-      'completedAt' => task.completed_at
-    }
-
-    render(json: serialized_task)
+    render(json: TaskSerializer.new(task, user).as_json)
   end
 
   def create
     task = user.tasks.new(create_task_params.merge(status: :pending))
 
     if task.save
-      serialized_task = {
-        'id' => task.id,
-        'userId' => user.id,
-        'status' => task.status,
-        'name' => task.name,
-        'createdAt' => task.created_at,
-        'completedAt' => task.completed_at
-      }
-
-      render(status: :created, json: serialized_task)
+      render(status: :created, json: TaskSerializer.new(task, user).as_json)
     else
-      serialized_error = {
-        'error' => {
-          'message' => 'Validation error',
-          'details' => task.errors.full_messages
-        }
-      }
-
-      render(status: :unprocessable_entity, json: serialized_error)
+      render(status: :unprocessable_entity, json: ErrorSerializer.new(task.errors.full_messages).as_json)
     end
   end
 
@@ -64,8 +28,7 @@ class TasksController < ApplicationController
 
     TaskCompleter.call(task:)
 
-    serialized_task = TaskSerializer.new(task, user).as_json
-    render(json: serialized_task)
+    render(json: TaskSerializer.new(task, user).as_json)
   end
 
   private
